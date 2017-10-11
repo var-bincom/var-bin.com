489eea5e75c2631bff184ba1e43c26bc143e6aec
var-bin <var-bin@users.noreply.github.com>
2017-10-11 22:09:15 +0300

    Working with try/catch, throw

--- docs/70480/l5/main.js
+++ docs/70480/l5/main.js
@@ -7,12 +7,44 @@
     console.info("onLoadHandler");
 
     workWithArray();
+    try {
+      ball();
+    } catch (error) {
+      console.error(error);
+    }
+
   }
 
   function workWithArray() {
     let arr = ["a", "b", "c"];
 
-    ar.push("d");
+    try {
+      arr.push("d");
+    } catch (error) {
+      const errorMessage = error.message;
+      const errorNumber = error.number;
+      const errorName = error.name;
+
+      console.error(`Message: ${errorMessage}\nNumber: ${errorNumber}\nType: ${errorName}`);
+    } finally {
+      console.log("finally block");
+    }
+
+    console.log(arr);
+  }
+
+  function ball() {
+    const ball = {
+      x: -1,
+      y: -1,
+      draw: function DrawBall(c) {
+        if (this.x < 0) {
+          throw new SyntaxError("Invalid X coordinate");
+        }
+      }
+    }
+
+    ball.draw();
   }
 
   global.addEventListener("load", onLoadHandler, false);
