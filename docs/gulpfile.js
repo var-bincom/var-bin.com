4c4e07d1f3219bc220b379ee21cdc081cc44e580
var-bin <var-bin@users.noreply.github.com>
2018-02-01 22:25:55 +0200

    Added new tasks

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -6,18 +6,23 @@ const gulp = require("gulp");
 const browserSync = require("browser-sync").create();
 const htmlmin = require("gulp-htmlmin");
 const rename = require("gulp-rename");
+const less = require("gulp-less");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const csso = require("postcss-csso");
 
 const ASSETS_DIR = path.resolve(__dirname, "./assets");
 const INDEX = path.resolve(__dirname, "./index.html");
 const INDEX_TPL = path.resolve(__dirname, "./index.tpl.html");
 const BASE_DIR = path.resolve("./");
+const STYLES = path.join(ASSETS_DIR, "less", "styles.less");
+const ASSETS_STYLES = path.join(ASSETS_DIR, "css");
 
 // Static server
 gulp.task("browser-sync", (cb) => {
   browserSync.init({
     server: BASE_DIR,
     files: [
-      ASSETS_DIR,
       INDEX
     ],
     open: false
@@ -35,3 +40,25 @@ gulp.task("htmlmin", () => {
     .pipe(rename("index.html"))
     .pipe(gulp.dest(BASE_DIR));
 });
+
+// less, css
+gulp.task("css", () => {
+  return gulp.src(STYLES)
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer
+    ]))
+    .pipe(gulp.dest(ASSETS_STYLES));
+});
+
+// css min
+gulp.task("cssmin", () => {
+  return gulp.src(path.join(ASSETS_STYLES, "styles.css"))
+    .pipe(postcss([
+      csso
+    ]))
+    .pipe(rename("styles.min.css"))
+    .pipe(gulp.dest(ASSETS_STYLES));
+});
+
+gulp.task("styles", gulp.series("css", "cssmin"));
