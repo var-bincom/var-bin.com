cb7c8a1ddf14647635fe5351bcf7af8953a35880
var-bin <var-bin@users.noreply.github.com>
2018-02-01 17:47:00 +0200

    Added task for minification html

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -1,15 +1,19 @@
 // gulpfile.js
 
+const path = require("path");
+
 const gulp = require("gulp");
 const browserSync = require("browser-sync").create();
-const path = require("path");
+const htmlmin = require("gulp-htmlmin");
+const rename = require("gulp-rename");
 
 const ASSETS_DIR = path.resolve(__dirname, "./assets");
 const INDEX = path.resolve(__dirname, "./index.html");
+const INDEX_TPL = path.resolve(__dirname, "./index.tpl.html");
 const BASE_DIR = path.resolve("./");
 
 // Static server
-gulp.task("browser-sync", () => {
+gulp.task("browser-sync", (cb) => {
   browserSync.init({
     server: BASE_DIR,
     files: [
@@ -18,4 +22,16 @@ gulp.task("browser-sync", () => {
     ],
     open: false
   });
+
+  cb();
+});
+
+// html min
+gulp.task("htmlmin", () => {
+  return gulp.src(INDEX_TPL)
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(rename("index.html"))
+    .pipe(gulp.dest(BASE_DIR));
 });
