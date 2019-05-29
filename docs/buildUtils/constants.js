07716a236901d42dbc2704da1c21bf6976bca56d
var-bin <var-bin@users.noreply.github.com>
2019-05-29 23:04:52 +0300

    Delete useless constants. Export constants in nodejs way

--- docs/buildUtils/constants.js
+++ docs/buildUtils/constants.js
@@ -1,20 +1,24 @@
-export const ASSETS_DIRECTORY = "assets";
-export const IMAGES_DIRECTORY = "images";
-export const SHOWER_DIRECTORY = "shower";
-export const PICTURES_DIRECTORY = "pictures";
-export const STYLES_DIRECTORY = "styles";
-export const STYLES_CSS = "styles.css";
-export const INDEX_HTML = "index.html";
-export const IMAGES_REGEXP = "/*.{png,jpg,JPG,jpeg,svg,gif}";
-export const STYLES_MIN_CSS = "styles.min.css";
-export const IMAGE_MIN_SUFFIX = ".min";
+const ASSETS_DIRECTORY = "assets";
+const IMAGES_DIRECTORY = "images";
+const SHOWER_DIRECTORY = "shower";
+const PICTURES_DIRECTORY = "pictures";
+const STYLES_DIRECTORY = "styles";
 
-const ASSETS_DIR = path.resolve(__dirname, "./assets");
-const INDEX = path.resolve(__dirname, "./index.html");
-const INDEX_TPL = path.resolve(__dirname, "./index.tpl.html");
-const BASE_DIR = path.resolve("./");
-const STYLES = path.join(ASSETS_DIR, "less", "styles.less");
-const ASSETS_STYLES = path.join(ASSETS_DIR, "css");
-const ASSETS_IMAGES = path.join(ASSETS_DIR, "images");
-const ASSETS_SVG = path.join(ASSETS_IMAGES, "/*.svg");
-const SVG_SPRITE = path.join(ASSETS_IMAGES, "sprite");
+const STYLES_CSS = "styles.css";
+const INDEX_HTML = "index.html";
+const IMAGES_REGEXP = "/*.{png,jpg,JPG,jpeg,svg,gif}";
+const STYLES_MIN_CSS = "styles.min.css";
+const IMAGE_MIN_SUFFIX = ".min";
+
+module.exports = {
+  ASSETS_DIRECTORY,
+  IMAGES_DIRECTORY,
+  SHOWER_DIRECTORY,
+  PICTURES_DIRECTORY,
+  STYLES_DIRECTORY,
+  STYLES_CSS,
+  INDEX_HTML,
+  IMAGES_REGEXP,
+  STYLES_MIN_CSS,
+  IMAGE_MIN_SUFFIX
+}
