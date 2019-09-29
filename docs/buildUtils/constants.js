ba4430491c5bc9e32ede1ab16fcc5b893f865760
var-bin <var-bin@users.noreply.github.com>
2019-09-29 10:54:38 +0300

    build: add new constants. Fix ESLint errors

--- docs/buildUtils/constants.js
+++ docs/buildUtils/constants.js
@@ -1,14 +1,17 @@
-const ASSETS_DIRECTORY = "assets";
-const IMAGES_DIRECTORY = "images";
-const SHOWER_DIRECTORY = "shower";
-const PICTURES_DIRECTORY = "pictures";
-const STYLES_DIRECTORY = "styles";
+const ASSETS_DIRECTORY = 'assets';
+const IMAGES_DIRECTORY = 'images';
+const SHOWER_DIRECTORY = 'shower';
+const PICTURES_DIRECTORY = 'pictures';
+const STYLES_DIRECTORY = 'styles';
 
-const STYLES_CSS = "styles.css";
-const INDEX_HTML = "index.html";
-const IMAGES_REGEXP = "/*.{png,jpg,JPG,jpeg,svg,gif}";
-const STYLES_MIN_CSS = "styles.min.css";
-const IMAGE_MIN_SUFFIX = ".min";
+const STYLES_CSS = 'styles.css';
+const INDEX_HTML = 'index.html';
+const INDEX_TEMPLATE_HTML = 'index.tpl.html';
+const IMAGES_REGEXP = '/*.{png,jpg,JPG,jpeg,gif}';
+const SVG_REGEXP = '/*.svg';
+const STYLES_MIN_CSS = 'styles.min.css';
+const IMAGE_MIN_SUFFIX = '.min';
+const ONE_LEVEL_UP = '../';
 
 module.exports = {
   ASSETS_DIRECTORY,
@@ -18,7 +21,10 @@ module.exports = {
   STYLES_DIRECTORY,
   STYLES_CSS,
   INDEX_HTML,
+  INDEX_TEMPLATE_HTML,
   IMAGES_REGEXP,
+  SVG_REGEXP,
   STYLES_MIN_CSS,
-  IMAGE_MIN_SUFFIX
-}
+  IMAGE_MIN_SUFFIX,
+  ONE_LEVEL_UP,
+};
